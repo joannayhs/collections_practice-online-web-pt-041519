044ad431a9959919f737bc7186090205fee1c200
Joanna Yuhas <joannayuhas@gmail.com>
2019-04-16 19:42:39 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,32 @@
 
+def sort_array_asc(array)
+  array.sort
+end 
+
+def sort_array_desc(array)
+  array.sort{|a, b| b <=> a}
+end 
+
+def sort_array_char_count(array)
+  array.sort{|a, b| a.length <=> b.length}
+end 
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end 
+
+def reverse_array(array)
+  array.reverse
+end 
+
+def kesha_maker(array)
+  new_array = []
+  array.each do |name|
+    new_array << name[2] = "$"
+  end
+end 
+
+def find_a(array)
+  
+end
